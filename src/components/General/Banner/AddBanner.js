233cb643d46e75a2226e191f597665277a9b02c8
Chirag <chiragsonar1718@gmail.com>
2023-03-16 15:17:30 +0530

    made Banner component

--- src/components/General/Banner/AddBanner.js
+++ src/components/General/Banner/AddBanner.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+// MUI
+import { Button, InputAdornment, MenuItem, TextField } from "@mui/material";
+import { Stack } from "@mui/system";
+
+// Api import
+import EcomAPI from "../../apis/EcomAPI";
+
 const AddBanner = () => {
   return (
     <div>AddBanner</div>
